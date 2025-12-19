e14232e20357f8a48f66089450816095bc4ce271
Mahalakshmi-4 <mahag0004@gmail.com>
2025-12-19 17:53:05 +0530

    static committed

--- com/tnsif/nonaccessmodifiers/Final.java
+++ com/tnsif/nonaccessmodifiers/Final.java
@@ -2,7 +2,7 @@ package com.tnsif.nonaccessmodifiers;
 
 public class Final {
 	final int x=200;
-	void print() {
+	final void print() {
 		System.out.println("X is :"+x);
 	}
 	public static void main(String[]args) {
@@ -10,6 +10,13 @@ public class Final {
 		f.print();
 		//f.x =300; cannot change the value as the final keyword is used
 		f.print();
+	}
+	
+	class Example extends Final{
+		void print() {
+			
+		}
+		
 	}
 	
 
